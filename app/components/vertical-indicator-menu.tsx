aa3fc13ae501707aae84aa2b0e6a930a9b8d9bf1
Reyhaneh <reyhanehkhoshghadam@gmail.com>
2024-11-07 15:06:01 +0330

    adds click functionality to vertical indicator menu

--- app/components/vertical-indicator-menu.tsx
+++ app/components/vertical-indicator-menu.tsx
@@ -7,9 +7,11 @@ import {
   faLanguage,
   faProjectDiagram,
   faCode,
+  IconDefinition,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo, useCallback, useRef } from "react";
+
 type SectionId =
   | "header"
   | "about"
@@ -19,9 +21,19 @@ type SectionId =
   | "languages"
   | "projects";
 
+const sectionsInfo: Array<{ icon: IconDefinition; id: SectionId }> = [
+  { icon: faCode, id: "header" },
+  { icon: faUser, id: "about" },
+  { icon: faLaptopCode, id: "skills" },
+  { icon: faBriefcase, id: "experience" },
+  { icon: faCertificate, id: "certifications" },
+  { icon: faLanguage, id: "languages" },
+  { icon: faProjectDiagram, id: "projects" },
+];
+
 const useInView = () => {
-  const [inView, setInView] = useState<SectionId>("header");
-  const [ratio, setRatio] = useState<Record<SectionId, number>>({
+  const [inViewSection, setInViewSection] = useState<SectionId>("header");
+  const ratios = useRef<Record<SectionId, number>>({
     header: 1,
     about: 0,
     skills: 0,
@@ -30,112 +42,89 @@ const useInView = () => {
     languages: 0,
     projects: 0,
   });
+  const sections = useRef<Record<string, Element | null>>();
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setRatio((ratio) => ({
-              ...ratio,
-              [entry.target.id]: entry.intersectionRatio,
-            }));
-            if (entry.intersectionRatio > ratio[inView]) {
-              setInView(entry.target.id as SectionId);
-            }
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
+    sections.current = {
+      header: document.querySelector("#header"),
+      about: document.querySelector("#about"),
+      skills: document.querySelector("#skills"),
+      experience: document.querySelector("#experience"),
+      certifications: document.querySelector("#certifications"),
+      languages: document.querySelector("#languages"),
+      projects: document.querySelector("#project"),
+    };
+  }, []);
 
-    const header = document.querySelector("#header");
-    const about = document.querySelector("#about");
-    const skills = document.querySelector("#skills");
-    const experience = document.querySelector("#experience");
-    const certifications = document.querySelector("#certifications");
-    const languages = document.querySelector("#languages");
-    const projects = document.querySelector("#project");
+  const observerCallBack = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          ratios.current[entry.target.id as SectionId] =
+            entry.intersectionRatio;
+          console.log(ratios.current);
 
-    const sections = [
-      header,
-      about,
-      skills,
-      experience,
-      certifications,
-      languages,
-      projects,
-    ];
-
-    for (const component of sections) {
-      if (component) {
-        observer.observe(component);
+          setInViewSection(
+            Object.entries(ratios.current).reduce(
+              (prev, curr) => {
+                const el = curr[1] > prev[1] ? curr : prev;
+                return el;
+              },
+              ["header", ratios.current.header]
+            )[0] as SectionId
+          );
+        }
+      });
+    },
+    [ratios.current, inViewSection]
+  );
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(observerCallBack, {
+        threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+      }),
+    [observerCallBack]
+  );
+  useEffect(() => {
+    if (sections.current) {
+      for (const component of Object.values(sections.current)) {
+        if (component) {
+          observer.observe(component);
+        }
       }
     }
-
     return () => {
-      for (const component of sections) {
-        if (component) {
-          observer.unobserve(component);
+      if (sections.current) {
+        for (const component of Object.values(sections.current)) {
+          if (component) {
+            observer.unobserve(component);
+          }
         }
       }
     };
-  }, [inView, ratio]);
-  return inView;
+  }, [observerCallBack, sections]);
+
+  return { inViewSection, sections };
 };
 export const VerticalIndicator = () => {
-  const inView = useInView();
+  const { inViewSection, sections } = useInView();
   return (
     <div className="fixed p-9 left-0 h-96 w-1/12  text-text top-1/2 border border-secondary rounded-full shadow-xl shadow-primary bg-background z-30 flex flex-col gap-7  items-center">
-      <FontAwesomeIcon
-        icon={faCode}
-        className={
-          inView === "header" ? "scale-[270%] transition-all" : "transition-all"
-        }
-      />
-      <FontAwesomeIcon
-        icon={faUser}
-        className={
-          inView === "about" ? "scale-[270%] transition-all" : "transition-all"
-        }
-      />
-      <FontAwesomeIcon
-        icon={faLaptopCode}
-        className={
-          inView === "skills" ? "scale-[300%] transition-all" : "transition-all"
-        }
-      />
-      <FontAwesomeIcon
-        icon={faBriefcase}
-        className={
-          inView === "experience"
-            ? "scale-[270%] transition-all"
-            : "transition-all"
-        }
-      />
-      <FontAwesomeIcon
-        icon={faCertificate}
-        className={
-          inView === "certifications"
-            ? "scale-[270%] transition-all"
-            : "transition-all"
-        }
-      />
-      <FontAwesomeIcon
-        icon={faLanguage}
-        className={
-          inView === "languages"
-            ? "scale-[270%] transition-all"
-            : "transition-all"
-        }
-      />
-      <FontAwesomeIcon
-        icon={faProjectDiagram}
-        className={
-          inView === "projects"
-            ? "scale-[270%] transition-all"
-            : "transition-all"
-        }
-      />
+      {sectionsInfo.map(({ icon, id }) => {
+        return (
+          <FontAwesomeIcon
+            icon={icon}
+            key={id}
+            className={
+              inViewSection === id
+                ? "scale-[270%] transition-all"
+                : "transition-all"
+            }
+            onClick={() => {
+              sections.current?.[id]?.scrollIntoView({ behavior: "smooth" });
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
