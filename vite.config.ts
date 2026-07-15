ee8155ad010ccba6d7ca77c8ca525231b7096ea9
Reyhaneh Khoshghadam <reyhanehkhoshghadam@gmail.com>
2026-07-15 12:54:01 +0330

    adds svgr plugin to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
+const svgrCommonOptions = {
+  jsxRuntime: "automatic" as const,
+  svgo: true,
+  plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+  svgoConfig: {
+    floatPrecision: 2,
+  },
+  //Accessibility
+  titleProp: true,
+  descProp: true,
+  svgProps: {
+    role: "img",
+  },
+};
 export default defineConfig({
   build: { target: "ES2024" },
   resolve: {
     tsconfigPaths: true,
   },
-  root: process.cwd(),
+  plugins: [
+    svgr({
+      svgrOptions: {
+        ...svgrCommonOptions,
+        icon: true,
+        dimensions: false,
+      },
+      include: "src/assets/icons/**/*.svg?react",
+    }),
+    svgr({
+      svgrOptions: {
+        ...svgrCommonOptions,
+      },
+      include: "src/assets/svgs/**/*.svg?react",
+    }),
+  ],
 });
