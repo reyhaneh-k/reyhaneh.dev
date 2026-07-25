7c25440a0319c7b0ee517115922d39a2df33de4e
Reyhaneh Khoshghadam <reyhanehkhoshghadam@gmail.com>
2026-07-26 02:11:37 +0330

    feat(layouts): adds grain!

--- src/routes/_app/writing/index.tsx
+++ src/routes/_app/writing/index.tsx
@@ -9,10 +9,8 @@ export const Route = createFileRoute("/_app/writing/")({
 
 function RouteComponent() {
   return (
-    <div>
-      {Array.from({ length: 100 }).map((_, index) => (
-        <div key={index}>{index}</div>
-      ))}
+    <div className="h-[10000px]">
+      <h1>Writing</h1>
     </div>
   );
 }
